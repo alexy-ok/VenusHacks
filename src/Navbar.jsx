8c87c2268ee84a1944d306e1c48ff1ab09bd1aa0
alexy-ok <alexanderyin5280@gmail.com>
2024-05-25 13:55:47 -0700

    added homepage

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './Navbar.css'
+import React from 'react'
+
+function Navbar(){
+    return(
+        <header>
+            <div class = 'nav-links'>
+            <nav>
+                <ul>
+                    <li><img src=''></img></li>
+                    <li href='/#home'>Home</li>
+                    <li>About</li>
+                    <li>Services</li>
+                </ul>
+            </nav>
+            </div>
+        </header>
+    )
+
+} 
+export default Navbar;
